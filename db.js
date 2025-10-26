f863a4b7964f46e0d6b83771e9dd32d4682f1d75
amalsodharan <amalsodharan789@gmail.com>
2025-10-26 19:53:08 +0530

    changes on mysql connect

--- db.js
+++ db.js
@@ -9,7 +9,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASSWORD,
   {
-    dialect: 'mariadb',
+    dialect: 'mysql',
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialectOptions: { decimalNumbers: true },
